92cfdec4565cc71ce1f634300f7b446f0e18a1c6
j m <1406993111@qq.com>
2024-05-11 17:46:49 +0800

    提交

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
             -
           </span>
           {{ fullYear }}
-          <a href="https://www.sw0.top">{{ siteAnthor }}</a>
+          <a target="_blank" href="https://www.sw0.top">{{ siteAnthor }}</a>
         </span>
         <!-- 站点备案 -->
         <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
