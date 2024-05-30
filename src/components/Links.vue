4a54fcb6333153541eb955ae9e80781e0a31aa64
j m <1406993111@qq.com>
2024-05-31 07:22:38 +0800

    提交

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -28,7 +28,6 @@
               @click="jumpLink(item)"
             >
               <Icon size="26">
-<!--                <component :is="siteIcon[item.icon]" />-->
                 <img class="icon" :src="item.icon" :width="26" :height="26" />
               </Icon>
               <span class="name text-hidden">{{ item.name }}</span>
